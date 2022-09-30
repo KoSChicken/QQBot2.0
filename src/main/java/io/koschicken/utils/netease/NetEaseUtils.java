ae811252a268f4b3af9de128ae38fc14551c4993
KoSChicken <hd9421@outlook.com>
2022-09-30 15:20:08 +0800

    修复点歌的空指针

--- src/main/java/io/koschicken/utils/netease/NetEaseUtils.java
+++ src/main/java/io/koschicken/utils/netease/NetEaseUtils.java
@@ -9,9 +9,13 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 public class NetEaseUtils {
+
+    private NetEaseUtils() {
+    }
 
     public static NetEaseMusic JSON2Song(String json) {
         JSONObject jsonObject = JSONObject.parseObject(json);
@@ -20,33 +24,20 @@ public class NetEaseUtils {
             return null;
         }
         JSONObject result = jsonObject.getJSONObject("result");
-        JSONArray songs = result.getJSONArray("songs");
-        NetEaseMusic netEaseMusic = songs.getObject(0, NetEaseMusic.class);
-        JSONObject songJsonObject = songs.getJSONObject(0);
-        JSONArray artists = songJsonObject.getJSONArray("ar");
-        List<Artist> artistList = new ArrayList<>();
-        for (int i = 0; i < artists.size(); i++) {
-            artistList.add(artists.getObject(i, Artist.class));
+        if (Objects.nonNull(result)) {
+            JSONArray songs = result.getJSONArray("songs");
+            NetEaseMusic netEaseMusic = songs.getObject(0, NetEaseMusic.class);
+            JSONObject songJsonObject = songs.getJSONObject(0);
+            JSONArray artists = songJsonObject.getJSONArray("ar");
+            List<Artist> artistList = new ArrayList<>();
+            for (int i = 0; i < artists.size(); i++) {
+                artistList.add(artists.getObject(i, Artist.class));
+            }
+            netEaseMusic.setArtists(artistList);
+            netEaseMusic.setAlbum(songJsonObject.getObject("al", Album.class));
+            return netEaseMusic;
+        } else {
+            return null;
         }
-        netEaseMusic.setArtists(artistList);
-        netEaseMusic.setAlbum(songJsonObject.getObject("al", Album.class));
-        return netEaseMusic;
-    }
-
-
-    public String source(String id) {
-        return String.format("http://music.163.com/song/media/outer/url?id=%s.mp3", id);
-    }
-
-    public static void main(String[] args) throws Exception {
-//        String json = FileUtils.readFileToString(new File("D://1.txt"), StandardCharsets.UTF_8);
-//        Song song = JSON2Song(json);
-//        CatCodeUtil catCodeUtil = CatCodeUtil.getInstance();
-//        String music = catCodeUtil.toCat("music", "kind=neteaseCloud",
-//                "title=" + song.getName(), "jumpUrl=" + song.getUrl(),
-//                "summary=" + song.getArtists().get(0).getName());
-//        System.out.println(music);
-        NetEaseMusic netEaseMusic = NetEaseMusic.searchWithoutLink("Don't Deal with the Devil cuphead", 1, 1);
-        System.out.println(netEaseMusic);
     }
 }
